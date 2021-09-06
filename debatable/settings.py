bc5007c154057cf56e8a802f1242cdd3cd309063
zucalerega <85199881+zucalerega@users.noreply.github.com>
2021-09-06 09:07:57 -0500

    change database

--- debatable/settings.py
+++ debatable/settings.py
@@ -81,10 +81,14 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': os.environ.get('QOVERY_POSTGRESQL_Z6BC6B886_DEFAULT_DATABASE_NAME', 'postgres'),
+    'USER': os.environ.get('QOVERY_POSTGRESQL_Z6BC6B886_LOGIN', 'postgres'),
+    'PASSWORD': os.environ.get('QOVERY_POSTGRESQL_Z6BC6B886_PASSWORD'),
+    'HOST': os.environ.get('QOVERY_POSTGRESQL_Z6BC6B886_HOST', 'z6bc6b886-postgresql.||Q_DOMAIN||'),
+    'PORT': os.environ.get('QOVERY_POSTGRESQL_Z6BC6B886_PORT', 5432),
+  }
 }
 
 
